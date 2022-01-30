c48819c7c29d8462c0354b965b63e7a772fe4448
Administrator <farhan.sajid@gourmetpakistan.com>
2022-01-30 21:25:09 +0500

    Worked on fragments

--- app/src/main/java/com/example/moviesdemo/activities/MainActivity.kt
+++ app/src/main/java/com/example/moviesdemo/activities/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.moviesdemo.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.moviesdemo.BuildConfig
 import com.example.moviesdemo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("NApi_key", "onCreate: "+resources.getString(R.string.api_key))
+
+
     }
 }
---
+++ app/src/main/java/com/example/moviesdemo/interfaces/ItemClickListener.kt
@@ -0,0 +1,8 @@
+package com.example.tentwentyassignment.interfaces
+
+import com.example.tentwentyassignment.models.UpcomingResult
+
+
+interface ItemClickListener {
+    fun watchItemCLicked(movieId : Int)
+}
